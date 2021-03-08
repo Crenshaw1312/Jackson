8791e622a6bcfa403ccba56d96bf388c29fc01af
Crenshaw <lazerspam101@gmail.com>
2021-03-08 12:54:42 -0800

    Fox command

---
+++ commands/Fun/fox.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require("discord.js");
+const fetch = require('node-fetch');
+
+module.exports = {
+    name: "fox",
+    description: "Gives a random fox",
+    usage: "catfact",
+    groups: ["fun"],
+    DM: true,
+    aliases: [],
+    run: async (client, message, args) => {
+        let fox = (await fetch("https://randomfox.ca/floof/").then(response => response.json())).image;
+
+        if (!fox) return client.err(message, "Fox", "No fox returned, try again or get support");
+        const embed = new MessageEmbed()
+        .setTitle('Fox')
+        .setColor(0x4B0082)
+        .setImage(fox);
+        return message.reply(embed);
+    }
+}
