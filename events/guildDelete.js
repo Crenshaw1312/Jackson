34119de1e9e25e0f06c09dec1bcb05a6b9b2eaeb
Crenshaw <lazerspam101@gmail.com>
2021-03-09 09:53:34 -0800

    notifications

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -4,10 +4,6 @@ exports.run = async (client, guild) => {
     let channel = client.channels.cache.get("818899863940366406");
     const embed = new MessageEmbed()
         .setColor(0x4B0082)
-        .setTitle(`New Guild - ${guild.name}`);
+        .setTitle(`Left Guild - ${guild.name}`);
     channel.send(embed);
-
-    channel = guild.systemChannel;
-    if(!channel) return;
-    channel.send('Hello! my name is Jackson, my prefix is `jack`!\n-Crenshaw#1312');
 }
