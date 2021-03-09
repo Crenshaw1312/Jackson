c18f3267291da442e7e39cad12fb69d3873b124c
Crenshaw <lazerspam101@gmail.com>
2021-03-09 08:26:09 -0800

    Update moderaton commands

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,32 +1,32 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-    name: "ban",
+    name: "kick",
     description: "Bans given member",
-    usage: "ban <user>",
+    usage: "kick <user>",
     groups: ["moderation"],
     DM: false,
     aliases: [],
     run: async (client, message, args) => {
 
         const embed = new MessageEmbed()
-        .setTitle('ban')
+        .setTitle('Kick')
         .setColor(0x4B0082);
 
-        if (!args[0])return client.err(message, "ban", 'Please provide a user to ban');
+        if (!args[0])return client.err(message, "Kick", 'Please provide a user to kick');
 
         const member = message.guild.members.cache.get(args[0]) || message.mentions.members.first() || message.guild.members.cache.find(m => m.user.username.toLowerCase() === args[0].toLowerCase());
         
-        if (!member)return client.err(message, "ban", 'Could not find user');
+        if (!member)return client.err(message, "Kick", 'Could not find user');
 
         let reason = args[1];
-        if (!args[0])return client.err(message, "ban", 'Please provide a user to ban');
-        if (!member.banable)return client.err(message, "ban", 'Could not find user');
+        if (!args[0])return client.err(message, "Kick", 'Please provide a user to kick');
+        if (!member.kickable)return client.err(message, "Kick", 'Could not find user');
 
         const memberTag = member.user.tag;
 
-        member.ban({ reason: reason });
-        embed.setDescription(`baned \`${memberTag}\` for reason: \`${reason}\``);
+        member.kick({ reason: reason });
+        embed.setDescription(`Kicked \`${memberTag}\` for reason: \`${reason}\``);
         return message.channel.send(embed);
     }
 }
