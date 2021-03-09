e486e0653aaf7a5c2ae69acf376a75b5b65ed42e
Crenshaw <lazerspam101@gmail.com>
2021-03-09 09:18:41 -0800

    typos

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -15,15 +15,15 @@ module.exports = {
         .setTitle('ban')
         .setColor(0x4B0082);
 
-        if (!args[0])return client.err(message, "ban", 'Please provide a user to ban');
+        if (!args[0])return client.err(message, "Ban", 'Please provide a user to ban');
 
         const member = message.guild.members.cache.get(args[0]) || message.mentions.members.first() || message.guild.members.cache.find(m => m.user.username.toLowerCase() === args[0].toLowerCase());
         
-        if (!member)return client.err(message, "ban", 'Could not find user');
+        if (!member)return client.err(message, "Ban", 'Could not find user');
 
         let reason = args[1];
-        if (!args[0])return client.err(message, "ban", 'Please provide a user to ban');
-        if (!member.banable)return client.err(message, "ban", 'Could not find user');
+        if (!args[0])return client.err(message, "Ban", 'Please provide a user to ban');
+        if (!member.banable)return client.err(message, "Ban", 'Could not find user');
 
         const memberTag = member.user.tag;
 
