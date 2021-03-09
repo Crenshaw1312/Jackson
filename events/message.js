d4944db8176ffc0226debabd6524d859cfe5becd
Crenshaw <lazerspam101@gmail.com>
2021-03-08 21:29:30 -0800

    added sqlite support!

--- events/message.js
+++ events/message.js
@@ -1,3 +1,5 @@
+const sqlite = require("sqlite3").verbose();
+
 exports.run = async (client, message) => {
     if (message.author.bot || !message.content.startsWith(client.config.prefix)) return;
 
@@ -10,6 +12,10 @@ exports.run = async (client, message) => {
     if (!command) return message.reply("Command not found");
     else{ 
         try {
+            // database
+            let database = new sqlite.Database('./jacksonDB.db', sqlite.OPEN_READWRITE);
+            client.database = database
+
             // errors
             if (command.DM === false && !message.guild) return client.err(message, "DMs", "This command cannot be ran in DMs");
             if (command.DM === true && !message.guild) {
@@ -18,11 +24,11 @@ exports.run = async (client, message) => {
             }
             if (command.groups[0] == "nsfw" && !message.channel.nsfw) return client.err(message, "NSFW", "This is not a NSFW channel");
 
+            // no overrides or nsfw filters stopped the command, run normally
             console.log(`Ran (guild) ${command.name} \[${args.join(" ")}\]- ${message.author.username}#${message.author.discriminator} \(${message.author.id}\)`);
             command.run(client, message, args);
         } catch (err) {
-            client.channels.cache.get(client.config.logs.error).send({embed:{description:err}}).catch(console.error);
-            message.channel.send(`Command \`${command.name}\` Failed Executing, Contact Support.`);
+            console.log(err);
         }
     }
 }
--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,15 @@
+const sqlite = require("sqlite3").verbose();
+
 exports.run = async (client, reaction, message) => {
     console.log("Jackson is online");
+
+    // database
+    const database = new sqlite.Database('./jacksonDB.db', sqlite.OPEN_READWRITE || sqlite.OPEN_CREATE);
+    if (database) {
+        console.log("Jackson DB activated");
+    } else {
+        console.log("Jackson DB failed");
+    }
 
     // presence
     client.user.setPresence({
