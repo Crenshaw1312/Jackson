3a41e1d5849a1e5d2413a02800961fb0aace548b
Crenshaw <68561594+Crenshaw1312@users.noreply.github.com>
2021-03-10 08:57:34 -0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    token: "",
-    prefix: "",
-    youtubeToken: "",
-    geniusToken: "",
+    token: "ODE3MTEyMjU1MjM3NDU1OTMy.YEExFg.KZmqxPcDpWGCzmd4VHNw8Il2EdM",
+    prefix: "jack",
+    youtubeToken: "AIzaSyDqrUGStuNvVZ12njTl_4ArBN0dQhPYROw",
+    geniusToken: "s3SVZNVUvyDfnoUng9GU7T4NzgXCQUi2BvIzO7GxhIyNWSFN0tZMH_THRVLkEvVV",
     jellybeans: {
         maxGive: 3,
         maxTake: 1
