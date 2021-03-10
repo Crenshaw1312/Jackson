9e13a543303c19a21e8c41c04b6a020c7828099d
Crenshaw <68561594+Crenshaw1312@users.noreply.github.com>
2021-03-10 08:39:08 -0800

    config.js remove private data

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    token: "ODE3MTEyMjU1MjM3NDU1OTMy.YEExFg.KZmqxPcDpWGCzmd4VHNw8Il2EdM",
-    prefix: "jack",
-    youtubeToken: "AIzaSyDqrUGStuNvVZ12njTl_4ArBN0dQhPYROw",
-    geniusToken: "s3SVZNVUvyDfnoUng9GU7T4NzgXCQUi2BvIzO7GxhIyNWSFN0tZMH_THRVLkEvVV",
+    token: "",
+    prefix: "",
+    youtubeToken: "",
+    geniusToken: "",
     jellybeans: {
         maxGive: 3,
         maxTake: 1
