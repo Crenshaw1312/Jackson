a38a440cccfcea5f89aec7b5562e14a0ff4cdb59
Crenshaw <lazerspam101@gmail.com>
2021-03-11 11:25:20 -0800

    Form Data request - funcs.js
    There will be more data required in config later but for now I just needed something, so I took this from SO

--- funcs.js
+++ funcs.js
@@ -27,3 +27,39 @@ exports.fetchMessages = async function (channel, amount) {
     });
     return messagesFormatted;
 }
+
+// Form submit (for image host), yoinked from StackOverflow here: https://stackoverflow.com/questions/133925/javascript-post-request-like-a-form-submit
+function post_to_url(path, params, method) {
+    method = method || "post"; // Set method to post by default, if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    var addField = function( key, value ){
+        var hiddenField = document.createElement("input");
+        hiddenField.setAttribute("type", "hidden");
+        hiddenField.setAttribute("name", key);
+        hiddenField.setAttribute("value", value );
+
+        form.appendChild(hiddenField);
+    }; 
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            if( params[key] instanceof Array ){
+                for(var i = 0; i < params[key].length; i++){
+                    addField( key, params[key][i] )
+                }
+            }
+            else{
+                addField( key, params[key] ); 
+            }
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
