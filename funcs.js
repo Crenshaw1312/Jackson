78fd733796cb50d870e81b67c6cb3538f9ac0095
Crenshaw <lazerspam101@gmail.com>
2021-03-10 23:03:42 -0800

    Update funcs.js
    improved export

--- funcs.js
+++ funcs.js
@@ -1,17 +1,19 @@
 const database = require("quick.db");
 
 // choose, if in args use args
-exports.choose = function choose(args, options, not) {
+exports.choose = async (args, options, not) => {
     let choice = options[Math.floor(Math.random() * options.length)];
-    if (!args) return choice;
+    if (!args)
+        return choice;
     for (let option of options) {
-        if (args.find(arg => option == arg && not !== arg)) choice = option;
+        if (args.find(arg => option == arg && not !== arg))
+            choice = option;
     }
     return choice;
 }
 
 // add user to guild database with starting jelly beans of 0
-exports.createAccount = async function createAccount(message) {
+exports.createAccount = async (message) => {
     let user = message.mentions.users.first() || message.author;
     await database.set(`${message.guild.id}.${user.id}`, {"jellybeans": 0});
     return await database.get(`${message.guild.id}.${user.id}`);
