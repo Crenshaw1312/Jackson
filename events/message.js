fe01f999c4c8b5d8f89d2e71852c8de8c964a2ae
Crenshaw <lazerspam101@gmail.com>
2021-03-11 10:03:04 -0800

    typing - message.js
    The bot will now appear as if it is typing when a command is ran

--- events/message.js
+++ events/message.js
@@ -29,19 +29,22 @@ exports.run = async (client, message) => {
     let command = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
     if (!command) return message.reply("Command not found");
     else{ 
+        message.channel.startTyping();
         try {
             // errors and overrides
             if (command.DM === false && !message.guild) return client.err(message, "DMs", "This command cannot be ran in DMs");
             if (command.DM === true && !message.guild) {
                 console.log(`Ran (DM) ${command.name} \[${args.join(" ")}\]- ${message.author.username}#${message.author.discriminator} \(${message.author.id}\)`);
-                 return command.run(client, message, args);
+                command.run(client, message, args);
+                return message.channel.stopTyping();
             }
             if (command.groups[0] == "nsfw" && !message.channel.nsfw) return client.err(message, "NSFW", "This is not a NSFW channel");
             if (command.groups[0] == "owner" && message.author.id !== '766385575530856458') return client.err(message, "Crenshaw Only", "This command can only be run by the bot owner Crenshaw#1312");
 
             // no overrides or nsfw filters stopped the command, run normally
             console.log(`Ran (guild) ${command.name} \[${args.join(" ")}\]- ${message.author.username}#${message.author.discriminator} \(${message.author.id}\)`);
-            return command.run(client, message, args);
+            command.run(client, message, args);
+            return message.channel.stopTyping();
         } catch (err) {
             console.log(err);
         }
