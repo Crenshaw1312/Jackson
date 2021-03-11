aed584e4c79927e2fd043b6fc7bf73c22cc1227c
Crenshaw <lazerspam101@gmail.com>
2021-03-11 14:41:32 -0800

    Update waifu.js
    it's just waifu now...
    you can also specify the seed used to gen it so you can get the same waifu later if you want

--- commands/Fun/waifu.js
+++ commands/Fun/waifu.js
@@ -3,18 +3,20 @@ const fs = require("fs");
 const Waifu = require("waifulabs");
 
 module.exports = {
-    name: "randomwaifu",
-    description: "gets a random waifu",
-    usage: "randomwaifu",
+    name: "waifu",
+    description: "Gets a random waifu",
+    usage: "waifu [seed]",
     groups: ["fun"],
     DM: true,
-    aliases: ["randwaifu", "rwaifu"],
+    aliases: [],
     run: async (client, message, args) => {
-        // setup waifu stuph
-        let genKey = [];
-        for (i = 0; i < 17; i++) {
-            genKey.push(Math.floor(Math.random() * 100));
+        // make the seed generation
+        let genKey = Array(17).fill(Math.floor(Math.random() * (2^32-1)));
+        if (args[0]) {
+            genKey = Array(17).fill(Number(args[0]))
         }
+        if (!Waifu.isValidSeed(genKey)) return client.err(message, "Waifu", "An invalid generation seed was passed");
+
         const waifu = await Waifu.generateBigWaifu(genKey);
         let res = new MessageAttachment(Buffer.from(waifu.image, 'base64'));
         message.reply(res);
