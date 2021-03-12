4f01cb47dac492c9eb246e3f4ccb93fb9bd7283d
Crenshaw <lazerspam101@gmail.com>
2021-03-11 17:49:41 -0800

    Update about.js
    Typos, credits, and more

--- commands/Information/about.js
+++ commands/Information/about.js
@@ -11,7 +11,14 @@ module.exports = {
         const embed = new MessageEmbed()
         .setTitle('About')
         .setColor(0x4B0082)
-        .setDescription("Jackson is an information fetcher bot.It gets everything from youtube channels to reddit.\n\nJackson is the little boy in the pfp and his favourite candy are purple jelly beans, and so his imaniary friend, a gaint cat, Crenshaw,\n\nBot owner is Crenshaw#1312\nCredit: WaterNinja#2093 for truth command and teaching me.\nCredit: Devnote#0745 and Radeon team for helping me set up the bot and teaching me too.");
+        .setDescription("Jackson is an information fetcher bot." +
+        "It gets everything from youtube channels to reddit." + 
+        "\n\n**Why the name Jackson?**\nJackson is the little boy in the pfp and his favourite candy are purple jelly beans. " +
+        "The big black cat is Crenshaw, his imaginary friend that is there for him as he deals with homelessness." +
+        "Jackson also really likes facts, and animals, so to fit this my bot is an information fetcher and has plenty of animal commands too!" +
+        "\n\nBot owner is Crenshaw#1312\n\n**Credits**\nWaterNinja#2093 for truth, dare, and wyr commands" + 
+        "and teaching me.\nDevnote#0745 and Radeon team for helping me set up the bot and teaching me too.\n" +
+        "Specky#6281 for teaching me and the npm for the waifu stuff");
         return message.reply(embed);
     }
 }
