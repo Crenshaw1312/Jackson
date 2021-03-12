a8c5ac5f5d1e4375f3407d8b2fd3d4c00598e5ac
Crenshaw <lazerspam101@gmail.com>
2021-03-11 17:26:30 -0800

    OTTER
    Holy shit this was sooo stressful for some odd reason

---
+++ commands/Fun/otter.js
@@ -0,0 +1,26 @@
+const gis  = require("g-i-s");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "otter",
+    description: "Get an image of a random otter",
+    usage: "otter",
+    groups: ["fun"],
+    DM: true,
+    aliases: ["otot"],
+    run: async (client, message, args) => {
+        gis('otter', logResults);
+        async function logResults(error, results){
+            if (error)return client.err(message, "Otter", "No otters found");
+
+            let random = Math.floor(Math.random() * results.length);
+            let image = results[random].url
+
+            const embed = new MessageEmbed()
+            .setTitle('Otter')
+            .setImage(image)
+            .setColor(0x4B0082);
+            return message.reply(embed);
+        }
+    }
+}
