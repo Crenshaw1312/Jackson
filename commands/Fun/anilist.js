6a0215ab4c5740173bf419c075f03b5566e31b1d
Crenshaw <lazerspam101@gmail.com>
2021-04-06 12:28:49 -0700

    Bug FIxes - anilist.js

--- commands/Fun/anilist.js
+++ commands/Fun/anilist.js
@@ -9,7 +9,7 @@ module.exports = {
     groups: [""],
     DM: true,
     cooldown: {type: "map", time: 0},
-    aliases: ["ani"],
+    aliases: ["al"],
     run: async (client, message, args) => {
 
         if (args.length < 2) return client.err(message, "Anilist", `Please provide the correct arguements:\n\`\`\`\nanilist <manga/anime/m/a> <search>\n\`\`\``);
@@ -27,10 +27,21 @@ module.exports = {
            result = await Anilist.media.anime(result.media[0].id);
         }
         
-        console.log(result.rankings);
+        // no results
+        if (result.status === 404) return client.err(message, "Anilist", "No result, please try another search");
+
+        // setup for ranks, trailer, twitter
         let ranks = "";
         for (let rank of result.rankings) {
-            ranks += rank.type.toLowerCase() + ": " + rank.rank + "\n";
+            ranks += rank.context.toLowerCase() + ": " + rank.rank + "\n";
+        }
+        let twitter = "";
+        if (result.hashtag) {
+            twitter = ` | [Twitter: ${result.hashtag}](https://twitter.com/hashtag/${result.hashtag.slice(1)})`
+        }
+        let trailer = "";
+        if (result.trailer) {
+            trailer = `[Trailer](${result.trailer}) | `
         }
 
         const embed = new MessageEmbed()
@@ -40,7 +51,7 @@ module.exports = {
         .setColor(0x4B0082)
         .setDescription(result.description)
         .addFields(
-            {name: "_ _", value: `[Trailer](${result.trailer}) | [Banner Image](${result.bannerImage}) | [Twitter: ${result.hashtag}](https://twitter.com/hashtag/${result.hashtag.slice(1)})`, inline: false},
+            {name: "_ _", value: `${trailer}[Banner Image](${result.bannerImage})${twitter}`, inline: false},
             {name: `Quick Stats`, value: `Total Reviews: ${result.reviews.length}\nFavourited: ${result.favourites} times\nFormat: ${result.format}\nAdult: ${result.isAdult}\nStarted on: ${result.startDate.day}/${result.startDate.month}/${result.startDate.year} (d/m/y)\nTotal episodes: ${result.episodes} ~${result.duration}min long`, inline: true},
             {name: "Scores", value: `\On lists/Poplarity: ${result.popularity}\nAverage Score: ${result.averageScore}/100\nMean Score: ${result.meanScore}/100\n`, inline: true},
             {name: "Ranks", value: `${ranks}`, inline: false},
